aebed92a47dab0b31d724e3bffb7828751ed318e
Akanksha Tyagi <67955370+Akanksha-Tyagi@users.noreply.github.com>
2021-01-23 23:03:31 +0530

    Add files via upload

---
+++ ThrowExceptionDemo.java
@@ -0,0 +1,38 @@
+package Akanksha.labassignment4;
+import java.io.IOException;
+
+import Akanksha.labassignment4.InputException;
+
+class throwException
+{
+ throwException()
+ {
+	 
+ }
+ void throwException() throws InputException, IOException
+ {
+	 System.out.println("In throwException Method");
+	 throw new IOException("IOException Occurs");
+ }
+}
+
+
+public class ThrowExceptionDemo 
+{
+	public static void main(String[] args)
+	{
+		throwException ob=new throwException();
+		try {
+			ob.throwException();
+			throw new InputException("InputException Occurs");
+		} catch (InputException | IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+					}
+	}
+}
+
+/*Ans:
+1.At method it is showing warning : This method has a constructor name.
+2.At main when we call throwException method it is showing surround with multi catch
+*/
